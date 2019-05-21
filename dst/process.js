66e7eae30f5c232c688e62bd7379d2082289ec43
Sebastian Meier <contact@sebastianmeier.eu>
2019-05-21 18:46:56 +0200

    temp working

--- dst/process.js
+++ dst/process.js
@@ -2,42 +2,77 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const bodyParser = require("body-parser");
 const express = require("express");
-// import { Pool } from "pg";
-// const config = JSON.parse(fs.readFileSync("config.json", "utf8"));
-// const client = new Pool(config.db);
-// client.connect();
+const fs = require("fs");
+const pg_1 = require("pg");
+const config = JSON.parse(fs.readFileSync("config.json", "utf8"));
+const client = new pg_1.Pool(config.db);
+client.connect();
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+    //intercepts OPTIONS method
+    if ('OPTIONS' === req.method) {
+        //respond with 200
+        res.send(200);
+    }
+    else {
+        //move on
+        next();
+    }
 });
 app.use((req, res) => {
     res.setHeader("Content-Type", "text/plain");
     console.log(req.body);
-    res.end(JSON.stringify({ id: 1, image: "people.jpeg" }));
+    if ("body" in req && "data" in req.body) {
+        if (req.body.data.length >= 1) {
+            const rel = JSON.parse(JSON.stringify(req.body.data));
+            rel.forEach((body) => {
+                let minX = Number.MAX_VALUE;
+                let minY = Number.MAX_VALUE;
+                let maxX = -Number.MAX_VALUE;
+                let maxY = -Number.MAX_VALUE;
+                body.keypoints.forEach((point) => {
+                    if (point.position.x > maxX) {
+                        maxX = point.position.x;
+                    }
+                    if (point.position.y > maxY) {
+                        maxY = point.position.y;
+                    }
+                    if (point.position.x < minX) {
+                        minX = point.position.x;
+                    }
+                    if (point.position.y < minY) {
+                        minY = point.position.y;
+                    }
+                });
+                const width = maxX - minX;
+                const height = maxY - minY;
+                body.keypoints.forEach((point) => {
+                    point.position.x = (point.position.x - minX) / width;
+                    point.position.x = (point.position.x - minY) / height;
+                });
+            });
+            client.query(`UPDATE metadata SET hasPose = TRUE, absPose = '${req.body.data}', relPose = '${rel}' WHERE id = ${req.body.id}`);
+        }
+        client.query(`SELECT id, download FROM metadata WHERE download IS NOT NULL AND image_problem IS NULL AND id > ${req.body.id} ORDER BY id ASC LIMIT 1`)
+            .then((result) => {
+            if ("rows" in result && result.rows.length >= 1) {
+                res.end(JSON.stringify({ id: result.rows[0].id, image: result.rows[0].download }));
+            }
+            else {
+                res.end(JSON.stringify({ message: "we are done" }));
+            }
+        });
+    }
+    else {
+        res.end(JSON.stringify({ message: "empty" }));
+    }
 });
 app.listen(5678, () => {
-    console.log('Example app listening on port 5678!');
+    console.log("Example app listening on port 5678!");
 });
-// client.query(`SELECT id, download, value FROM metadata WHERE download IS NOT NULL AND image_problem = TRUE`)
-//     .then((result) => {
-//         result.rows.forEach((r) => {
-//             if (fs.existsSync(r.download) && fs.existsSync(r.download + ".bk.jpg")) {
-//                 const stats = fs.statSync(r.download);
-//                 const statsNew = fs.statSync(r.download + ".bk.jpg");
-//                 if (statsNew.size > stats.size) {
-//                     console.log(r.id);
-//                     const oldContent = fs.readFileSync(r.download);
-//                     client.query(`UPDATE metadata SET image_comment = '${oldContent}' WHERE id = ${r.id}`);
-//                     fs.renameSync(r.download + ".bk.jpg", r.download);
-//                     fs.unlink(r.download + ".bk.jpg", (err) => {
-//                         process.stdout.write(JSON.stringify(err));
-//                     });
-//                 }
-//             }
-//         });
-//     });
 //# sourceMappingURL=process.js.map
